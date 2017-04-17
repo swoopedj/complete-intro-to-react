43ab495cdaf9c873a711fa8f9f51260913fe2db7
Dylan Swoope <dylanswoope@Dylans-MacBook-Pro.local>
2017-04-17 10:01:55 -0500

    work through end of 'Filtering' vid

--- js/Search.js
+++ js/Search.js
@@ -3,14 +3,34 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default string'
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard key={show.imdbID} show={show} />
-          )
-        })}
+        <header>
+          <h1>svideo</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows
+            .filter((show) => {
+              return `${show.title} ${show.title}`
+                .toUpperCase().indexOf(this.state.searchTerm
+                .toUpperCase()) >= 0
+            })
+            .map((show) => {
+            return (
+              <ShowCard key={show.imdbID} {...show} />
+            )
+          })}
+        </div>
       </div>
     )
   }
--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,19 +1,18 @@
 import React from 'react'
 
-const {shape, string} = React.PropTypes
+const { string } = React.PropTypes
 
-const ShowCard = React.createClass ({
+const ShowCard = React.createClass({
   propTypes: {
-  show: shape({
     poster: string,
     title: string,
     year: string,
     description: string
-  }),
+  },
   render () {
-    const { poster, title, year, description} = this.props.show
+    const { poster, title, year, description } = this.props
     return (
-      <div className="show-card">
+      <div className='show-card'>
         <img src={`/public/img/posters/${poster}`} />
         <div>
           <h3>{title}</h3>
